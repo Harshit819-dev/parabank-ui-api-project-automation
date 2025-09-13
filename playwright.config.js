43c121754961abf558758abede6c83382af5084e
Harshit Jain <hash4692@gmail.com>
2025-09-14 01:17:38 +0530

    configuring CI/CD

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,13 @@ import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
+    timeout: 40 * 1000,  // per-test timeout
+    retries: 1,           // number of retries on failure
+    workers: 1,         // number of parallel workers
     reporter: [
         ['list'], // console output
         ['allure-playwright'], // allure results
-        ['html', { open: 'never' }], // Playwright HTML report
+        ['html', {open: 'never'}], // Playwright HTML report
     ],
     use: {
         baseURL: 'https://parabank.parasoft.com/', //Base URL for the tests
@@ -14,5 +17,14 @@ export default defineConfig({
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
-    }
+    },
+    projects: [{
+        name: 'UI Tests',
+        testDir: './tests/ui',  // UI folder runs first
+    },
+        {
+            name: 'API Tests',
+            testDir: './tests/api', // API folder runs after UI
+        },
+    ]
 });
